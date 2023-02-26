0f9a760330a29c36e2ae9101e80f6e5fb7304f16
Israel Parodi <israel.parodi@delfosti.com>
2023-02-26 17:27:49 -0500

    user module done

--- src/passenger/dto/create-passenger.dto.ts
+++ src/passenger/dto/create-passenger.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePassengerDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class CreatePassengerDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+}
